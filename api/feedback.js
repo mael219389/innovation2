dc72e4c65dc21cc6b8e34ed62336575710a9999e
mael219389 <mvignolet@yahoo.com>
2025-01-24 15:10:45 +0100

    MAJ

--- api/feedback.js
+++ api/feedback.js
@@ -1,21 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
-const saveFeedback = (req, res) => {
-    const feedback = req.body.feedback;
-    const feedbackPath = path.join(__dirname, '..', 'uploads', 'feedback.txt');
-
-    fs.appendFile(feedbackPath, `Feedback: ${feedback}\n`, (err) => {
-        if (err) {
-            return res.status(500).json({ message: 'Erreur lors de l\'enregistrement des commentaires' });
-        }
-        res.status(200).json({ message: 'Commentaire envoyé avec succès' });
-    });
-};
-
 module.exports = (req, res) => {
     if (req.method === 'POST') {
-        return saveFeedback(req, res);
+        const feedback = req.body.feedback;
+        const feedbackPath = path.join(__dirname, '..', 'uploads', 'feedback.txt');
+
+        fs.appendFile(feedbackPath, `Feedback: ${feedback}\n`, (err) => {
+            if (err) {
+                return res.status(500).json({ message: 'Erreur lors de l\'enregistrement des commentaires' });
+            }
+            res.status(200).json({ message: 'Commentaire envoyé avec succès' });
+        });
+    } else {
+        res.status(405).end(); // Méthode non autorisée
     }
-    res.status(405).end(); // Méthode non autorisée
 };
--- api/upload.js
+++ api/upload.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads');
+        cb(null, path.join(__dirname, '..', 'uploads')); // Destination ajustée
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname));
@@ -13,18 +13,15 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-const uploadFile = (req, res) => {
-    upload.single('file-upload')(req, res, (err) => {
-        if (err) {
-            return res.status(400).json({ message: 'Erreur lors de l\'envoi du fichier' });
-        }
-        res.status(200).json({ message: 'Fichier envoyé avec succès', file: req.file });
-    });
-};
-
 module.exports = (req, res) => {
     if (req.method === 'POST') {
-        return uploadFile(req, res);
+        upload.single('file-upload')(req, res, (err) => {
+            if (err) {
+                return res.status(400).json({ message: 'Erreur lors de l\'envoi du fichier' });
+            }
+            res.status(200).json({ message: 'Fichier envoyé avec succès', file: req.file });
+        });
+    } else {
+        res.status(405).end(); // Méthode non autorisée
     }
-    res.status(405).end(); // Méthode non autorisée
 };
